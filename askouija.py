9635f229eaedc5a22ad1146d1db7f7d98a77b677
blairly <blair.harper@gmail.com>
2018-12-15 10:45:55 +0000

    Changed to PRAW. Added simple code for rising posts and top answers.

--- askouija.py
+++ askouija.py
@@ -1,6 +1,4 @@
-import requests
-import requests.auth
-import json
+import praw
 
 print("IamAdeadbot 0.1 - Welcome to the spirit world. e: b@blairdev.com\n")
 
@@ -10,17 +8,26 @@ client_id = secrets[0].rstrip()
 client_secret = secrets[1].rstrip()
 client_username = secrets[2].rstrip()
 client_password = secrets[3].rstrip()
+ua = "deadbot/0.1 by blairdev.com"
 
-client_auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
-post_data = {"grant_type": "password", 
-    "username": client_username, 
-    "password": client_password}
-headers = {"User-Agent": "deadbot/0.1 by blairdev.com"}
-print("Authenticating...")
-response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
-json_data = json.loads(response.text)
+reddit = praw.Reddit(client_id=client_id,
+    client_secret=client_secret,
+    user_agent=ua,
+    username=client_username,
+    password=client_password)
 
-if 'error' in json_data:
-    print("Authentication failed. Please check credentials and internet connection.")
-else:
-    print("Authenticated!\n Access token: {0}".format(json_data['access_token']))
+ouija = reddit.subreddit('askouija')
+
+top_word = ""
+
+for submission in ouija.hot(limit=5):
+    if submission.id != "673qgu": # ignore the 'please read the rules blah blah blah' post
+        submission.comments.replace_more(limit=None)
+        submission.comment_sort = 'top'
+        print(submission.title)
+        top_level_comments = list(submission.comments)        
+        top_word = "{0}".format(top_level_comments[0].body)
+        for reply in top_level_comments[0].replies.list():
+            if (len(reply.body) == 1) and (reply.score > 0):
+                top_word = top_word + "{0}".format(reply.body)
+        print("{0}\n".format(top_word))
