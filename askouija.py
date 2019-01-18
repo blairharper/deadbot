c1b082a99142bd84fbc0f3d8e66ce4cced170cc1
Blair <b@blairdev.com>
2019-01-18 22:34:49 +0000

    implemented rnn/lstm. added function to generate questions/post to reddit.

--- askouija.py
+++ askouija.py
@@ -5,6 +5,8 @@ from database_setup import Base, Question
 from tqdm import tqdm
 from os import system
 import sys
+import deadbot_nn
+import pickle
 print("deadbot 0.1 - reddit.com/AskOuija AI bot e: b@blairdev.com\n")
 
 # Get secret login info etc, use rstrip to remove any whitespace
@@ -38,26 +40,57 @@ ouija = reddit.subreddit('askouija')
 ans = True
 
 
-def get_hot():
+def get_hot(askreddit=0):
     post_counter = 0
-    limit = int(input("How many posts to scan? "))
+    limit = int(input("How many posts to scan?"))
     print("\n")
+    if askreddit == 1:
+        ouija = reddit.subreddit('askreddit')
+    else:
+        ouija = reddit.subreddit('askouija')
+
     with tqdm(total=limit) as pbar:
-        for submission in ouija.top(limit=limit, time_filter='day'):
+        for submission in ouija.top(limit=limit, time_filter='all'):
             flairexists = submission.link_flair_text is not None
             answered = submission.link_flair_text != 'unanswered'
+
+            if askreddit == 1:
+                flairexists = True
+                answered = True
 
             if flairexists and answered:
                 # print(submission.link_flair_text[12:])
 
                 if session.query(Question.id).filter_by(submission_id=submission.id).scalar() is None:
 
+                    if submission.link_flair_text is not None:
+                        answer = submission.link_flair_text[12:]
+                    else:
+                        answer = "||"
                     new_question = Question(submission_id=submission.id,
                                             title=submission.title,
-                                            answer=submission.link_flair_text[12:])
+                                            answer=answer)
                     session.add(new_question)
                     session.commit()
                     post_counter += 1
+            pbar.update(1)
+    print("Done! {0} new posts added to the database.\n".format(post_counter))
+
+
+def get_new():
+    post_counter = 0
+    limit = int(input("How many posts to scan? "))
+    print("\n")
+    with tqdm(total=limit) as pbar:
+        for submission in ouija.new(limit=limit):
+            if session.query(Question.id).filter_by(submission_id=submission.id).scalar() is None:
+
+                new_question = Question(submission_id=submission.id,
+                                        title=submission.title,
+                                        answer="||")
+                session.add(new_question)
+                session.commit()
+                post_counter += 1
             pbar.update(1)
     print("Done! {0} new posts added to the database.\n".format(post_counter))
 
@@ -83,37 +116,105 @@ def get_stats():
 
 
 def preprocess_data():
-    # TODO:
-    # 1. Get every entry in DB
-    # 2. Give each unique word an ID number
-    # 3. replace every word with an ID number / create lookup table
-    #       (dictionaries -> word to ID and ID to word)
-    # 4. Tokenise punctuation
-    pass
+    questions = []
+    q = session.query(Question).all()
+
+    for post in q:
+        questions.append(post.title)
+
+    # convert DB query results to string
+    text = ''.join(questions)
+
+    token_dict = token_lookup()
+
+    # replace punctuation characters with token
+    for key, token in token_dict.items():
+        text = text.replace(key, ' {} '.format(token))
+
+    text = text.lower()
+    text = text.split()
+
+    # convert text to integer IDs
+    vocab_to_int, int_to_vocab = create_lookup_tables(text)
+    int_text = [vocab_to_int[word] for word in text]
+    pickle.dump((int_text, vocab_to_int, int_to_vocab, token_dict), open('preprocess.p', 'wb'))
+    print("Data preprocessed and saved.")
+
+
+def create_lookup_tables(text):
+    int_to_vocab, vocab_to_int = {}, {}
+
+    for x, y in enumerate(text):
+        int_to_vocab[x] = y
+        vocab_to_int[y] = x
+
+    return vocab_to_int, int_to_vocab
+
+
+def token_lookup():
+    """
+    Generate a dict to turn punctuation into a token.
+    :return: Tokenize dictionary where the key is the punctuation and the value is the token
+    """
+    d_punctuation = {
+        '.': '||Period||',
+        ',': '||Comma||',
+        '"': '||QuotationMark||',
+        ';': '||Semicolon||',
+        '!': '||ExclamationMark||',
+        '?': '||QuestionMark||',
+        '(': '||LeftParentheses||',
+        ')': '||RightParentheses||',
+        '--': '||Dash||',
+        '_': '||Underscore||',
+        '\n': '||Return||',
+    }
+    return d_punctuation
 
 
 while ans:
     print("""
         Deadbot Menu:
         1. Scan for hot posts
-        2. Show me what you've got
-        3. Stats
-        4. Preprocess data
-        5. Exit\n
+        2. Scan for new questions (may not have answers)
+        3. Show me what you've got
+        4. Stats
+        5. Preprocess data
+        6. Train model
+        7. Generate a questions from saved model
+        8. Exit
+        9. Scan ask reddit\n
         """)
     ans = input("What would you like to do? ")
     if ans == "1":
         cls()
         get_hot()
-    elif ans == "2":
+    if ans == "2":
         cls()
-        query_db()
+        get_new()
     elif ans == "3":
         cls()
-        get_stats()
+        query_db()
     elif ans == "4":
         cls()
-        preprocess_data()
+        get_stats()
     elif ans == "5":
+        cls()
+        preprocess_data()
+    elif ans == "6":
+        cls()
+        deadbot_nn.train()
+    elif ans == "7":
+        cls()
+        prime_word = input("What prime word should I use? ")
+        get_question = deadbot_nn.generate_question(prime_word.lower())
+        if get_question is not None:
+            print("Posting to reddit... {}".format(get_question))
+            new_submission = ouija.submit(title=get_question, selftext='')
+            print("Done! {}".format(new_submission.permalink))
+    elif ans == "8":
         print("\nGoodbye")
         sys.exit()
+    elif ans == "9":
+        cls()
+        get_hot(askreddit=1)
--- deadbot_nn.py
+++ deadbot_nn.py
@@ -1,1 +1,256 @@
 # Neural network for deadbot
+import pickle
+import tensorflow as tf
+from tensorflow.python.client import device_lib
+import numpy as np
+from tensorflow.contrib import seq2seq
+import re
+
+
+def load_data():
+    return pickle.load(open('preprocess.p', mode='rb'))
+
+def save_params(params):
+    pickle.dump(params, open('params.p', 'wb'))
+
+def load_params():
+    return pickle.load(open('params.p', mode='rb'))
+
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return (device.name for device in local_device_protos if device.device_type == 'GPU')
+
+
+def version_check():
+    print(get_available_gpus())
+    print(tf.__version__)
+
+
+def get_inputs():
+    inputs = tf.placeholder(tf.int32, shape=[None,None] , name='input')
+    targets = tf.placeholder(tf.int32, shape=[None,None], name='target')
+    learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+    return inputs, targets, learning_rate
+
+def get_init_cell(batch_size, rnn_size):
+    lstm = tf.nn.rnn_cell.LSTMCell(name='basic_lstm_cell', num_units=rnn_size)
+    drop = tf.contrib.rnn.MultiRNNCell(
+        [tf.contrib.rnn.DropoutWrapper(
+            lstm,
+            output_keep_prob=0.8)
+        ])
+    cell = tf.contrib.rnn.MultiRNNCell([drop])
+    ini = cell.zero_state(batch_size, tf.float32)
+    initial_state = tf.identity(ini, name='initial_state')
+    return cell, initial_state
+
+
+def get_embed(input_data, vocab_size, embed_dim):
+    embed = tf.Variable(tf.random_uniform((vocab_size, embed_dim), -1, 1))
+    embeded_input = tf.nn.embedding_lookup(embed, input_data)
+    return embeded_input
+
+
+def build_rnn(cell, inputs):
+    outputs, final_state = tf.nn.dynamic_rnn(cell, inputs, dtype=tf.float32)
+    final_state = tf.identity(final_state, name='final_state')
+    return outputs, final_state
+
+
+def build_nn(cell, rnn_size, input_data, vocab_size, embed_dim):
+    embed = get_embed(input_data, vocab_size, embed_dim)
+    output, final_state = build_rnn(cell, embed)
+    logits = tf.contrib.layers.fully_connected(output, vocab_size, activation_fn=None)
+
+    return logits, final_state
+
+
+def get_batches(int_text, batch_size, seq_length):
+    nb = int(len(int_text) / (batch_size * seq_length))
+    nb_k = (nb * batch_size * seq_length)
+
+    x_data = np.array(int_text[: nb_k])
+    y_data = np.array(int_text[1 : nb_k] + [int_text[0]])
+
+    x = x_data.reshape(batch_size, -1)
+    y = y_data.reshape(batch_size, -1)
+
+    x = np.split(x, nb, 1)
+    y = np.split(y, nb, 1)
+
+    batches = np.array(list(zip(x, y)))
+    batches = batches.reshape(nb, 2, batch_size, seq_length)
+
+    return batches
+
+
+def train():
+    version_check()
+    int_text, vocab_to_int, int_to_vocab, token_dict = load_data()
+
+    # hyperparams
+    num_epochs = 75
+    batch_size = 32
+    rnn_size = 1024
+    embed_dim = 128
+    seq_length = 19
+    learning_rate = 0.002
+    show_every_n_batches = 16
+    save_dir = './save'
+
+    train_graph = tf.Graph()
+    with train_graph.as_default():
+        vocab_size = len(int_to_vocab)
+        input_text, targets, lr = get_inputs()
+        input_data_shape = tf.shape(input_text)
+        cell, initial_state = get_init_cell(input_data_shape[0], rnn_size)
+        logits, final_state = build_nn(cell, rnn_size, input_text, vocab_size, embed_dim)
+
+        # Probabilities for generating words
+        probs = tf.nn.softmax(logits, name='probs')
+
+        # Loss function
+        cost = seq2seq.sequence_loss(
+            logits,
+            targets,
+            tf.ones([input_data_shape[0], input_data_shape[1]]))
+
+        # Optimizer
+        optimizer = tf.train.AdamOptimizer(lr)
+
+        # Gradient Clipping
+        gradients = optimizer.compute_gradients(cost)
+        capped_gradients = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gradients if grad is not None]
+        train_op = optimizer.apply_gradients(capped_gradients)
+
+    # Train
+    batches = get_batches(int_text, batch_size, seq_length)
+
+    with tf.Session(graph=train_graph) as sess:
+        sess.run(tf.global_variables_initializer())
+
+        for epoch_i in range(num_epochs):
+            state = sess.run(initial_state, {input_text: batches[0][0]})
+
+            for batch_i, (x, y) in enumerate(batches):
+                feed = {
+                    input_text: x,
+                    targets: y,
+                    initial_state: state,
+                    lr: learning_rate}
+                train_loss, state, _ = sess.run([cost, final_state, train_op], feed)
+
+                # Show every <show_every_n_batches> batches
+                if (epoch_i * len(batches) + batch_i) % show_every_n_batches == 0:
+                    print('Epoch {:>3} Batch {:>4}/{}   train_loss = {:.3f}'.format(
+                        epoch_i,
+                        batch_i,
+                        len(batches),
+                        train_loss))
+
+        # Save Model
+        saver = tf.train.Saver()
+        saver.save(sess, save_dir)
+
+        # Save Model
+        saver = tf.train.Saver()
+        saver.save(sess, save_dir)
+        params = (seq_length, save_dir)
+        save_params(params)
+        print('Model Trained and Saved')
+
+def get_tensors(loaded_graph):
+    x_tensor = loaded_graph.get_tensor_by_name('input:0')
+    ini_tensor = loaded_graph.get_tensor_by_name('initial_state:0')
+    fin_tensor = loaded_graph.get_tensor_by_name('final_state:0')
+    p_tensor = loaded_graph.get_tensor_by_name('probs:0')
+    return x_tensor, ini_tensor, fin_tensor, p_tensor
+
+
+def pick_word(probabilities, int_to_vocab):
+    csum = np.cumsum(probabilities)
+    c = np.sum(probabilities) * np.random.rand(1)
+    word = int_to_vocab[int(np.searchsorted(csum, c))]
+    return word
+
+def split_into_sentences(text):
+    alphabets = "([A-Za-z])"
+    prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+    suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+    starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+    acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+    websites = "[.](com|net|org|io|gov)"
+
+    text = " " + text + "  "
+    text = text.replace("\n"," ")
+    text = re.sub(prefixes,"\\1<prd>",text)
+    text = re.sub(websites,"<prd>\\1",text)
+    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
+    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
+    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
+    if "”" in text: text = text.replace(".”","”.")
+    if "\"" in text: text = text.replace(".\"","\".")
+    if "!" in text: text = text.replace("!\"","\"!")
+    if "?" in text: text = text.replace("?\"","\"?")
+    text = text.replace(".",".<stop>")
+    text = text.replace("?","?<stop>")
+    text = text.replace("!","!<stop>")
+    text = text.replace("<prd>",".")
+    sentences = text.split("<stop>")
+    sentences = sentences[:-1]
+    sentences = [s.strip() for s in sentences]
+    return sentences
+
+def generate_question(prime_word):
+    int_text, vocab_to_int, int_to_vocab, token_dict = load_data()
+    seq_length, load_dir = load_params()
+    gen_length = 1000
+
+    loaded_graph = tf.Graph()
+    with tf.Session(graph=loaded_graph) as sess:
+        loader = tf.train.import_meta_graph(load_dir + '.meta')
+        loader.restore(sess, load_dir)
+
+        input_text, initial_state, final_state, probs = get_tensors(loaded_graph)
+        gen_question = [prime_word]
+
+        prev_state = sess.run(initial_state, {input_text: np.array([[1]])})
+
+        for n in range(gen_length):
+            dyn_input = [[vocab_to_int[word] for word in gen_question[-seq_length:]]]
+            dyn_seq_length = len(dyn_input[0])
+
+            # Get Prediction
+            probabilities, prev_state = sess.run(
+                [probs, final_state],
+                {input_text: dyn_input, initial_state: prev_state})
+
+            pred_word = pick_word(probabilities[0][dyn_seq_length - 1], int_to_vocab)
+
+            gen_question.append(pred_word)
+
+    ouija_question = ' '.join(gen_question)
+
+    for key, token in token_dict.items():
+        ending = ' ' if key in ['\n', '(', '"'] else ''
+        ouija_question = ouija_question.replace(' ' + token.lower(), key)
+
+    ouija_question = ouija_question.replace('\n ', '\n')
+    ouija_question = ouija_question.replace('( ', '(')
+    ouija_question = split_into_sentences(ouija_question)
+
+    for question in ouija_question:
+        print(question)
+        post_to_reddit = input("Would you like to post this question to reddit? [N/y] ")
+
+        if post_to_reddit.lower() == 'y':
+            return question.capitalize()
+        print('\n')
+
+
+
